6e305421449dc01056bcaaa7b8346d2cd01df861
gorphensu <32945777@qq.com>
2018-11-07 12:51:31 +0800

    商品详情 添加规格

--- src/views/product/detail.vue
+++ src/views/product/detail.vue
@@ -18,6 +18,63 @@
           <span class="no">2</span>
           商品规格与库存
         </div>
+        <div class="content">
+          <div class="label-content">商品规格</div>
+          <div class="inner-content">
+            <div class="remark">最多添加两个商品规格，第一个商品规格可添加规格图片</div>
+            <br>
+            {{ form2 }}
+            <div v-for="(specItem, index) in form2.specs" :key="index" class="panel-container">
+              <div class="panel-header">
+                <el-select v-model="specItem.specType" size="mini">
+                  <el-option
+                    v-for="item in specOptions"
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id" />
+                </el-select>
+                &nbsp;
+                <img v-if="specItem.image" :src="specItem.image" class="spec-photo">
+                <el-upload
+                  :show-file-list="false"
+                  :on-success="specUploadSuccessHandler"
+                  :before-upload="beforeSpecUploadHandler(specItem)"
+                  class="spec-uploader"
+                  action="https://jsonplaceholder.typicode.com/posts/">
+                  <el-button v-show="!specItem.image" type="text">添加图片</el-button>
+                </el-upload>
+                <!-- <el-button v-show="!specItem.image" type="text">添加图片</el-button> -->
+                <el-button v-show="specItem.image" type="text">删除图片</el-button>
+                <el-button type="text" class="float-right" @click="deleteSpecTypeHandler(index)">删除规格</el-button>
+              </div>
+              <div class="panel-body">
+                <el-form ref="specForm" :model="specItem" :inline="true" size="mini">
+                  <el-form-item
+                    v-for="(spec, $index) in specItem.data"
+                    :key="$index"
+                    :rules="{ required: true, message: ' ', trigger: 'blur' }"
+                    :prop="'data.' + $index">
+                    <el-input v-model="specItem.data[$index]" />
+                    <i class="el-icon-error" @click="deleteSpecHandler(specItem.data, $index)"/>
+                  </el-form-item>
+                  <el-button type="text" @click="addSpecHandler(specItem, index, specItem.data)">添加</el-button>
+                </el-form>
+              </div>
+              <br>
+            </div>
+            <div>
+              <el-button type="default" @click="addSpecTypeHandler"><i class="el-icon-plus"/>添加规格</el-button>
+            </div>
+          </div>
+        </div>
+        <div class="content">
+          <div class="label-content">价格及库存</div>
+          <div class="inner-content">
+            <div class="panel-container">
+              12
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="block-item last-child">
@@ -76,6 +133,23 @@ import * as FeightAPI from '../../api/feight'
 export default {
   data() {
     return {
+      specOptions: [{
+        id: '1',
+        name: '尺寸'
+      }, {
+        id: '2',
+        name: '重量'
+      }, {
+        id: '3',
+        name: '颜色'
+      }],
+      form2: {
+        specs: [{
+          specType: '1',
+          image2: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1240685591,3219211103&fm=58',
+          data: ['55寸']
+        }]
+      },
       form3: {
         productType: '',
         isSecond: '0',
@@ -111,6 +185,9 @@ export default {
   created() {
     this.getFeightList()
   },
+  beforeDestroy() {
+    this._specItem = null
+  },
   methods: {
     saveHandler() {
     },
@@ -126,6 +203,45 @@ export default {
       }).catch(error => {
         console.error(error)
       })
+    },
+    addSpecHandler(specItem, index, datas) {
+      const formVm = this.$refs['specForm'] && this.$refs['specForm'][index]
+      if (formVm) {
+        formVm.validate(result => {
+          if (!result) return
+          datas.push('')
+        })
+      }
+    },
+    specUploadSuccessHandler(response, file, fileList) {
+      if (this._specItem) {
+        debugger
+        // TODO 需要返回url路径
+        this._specItem.image = file
+      }
+    },
+    beforeSpecUploadHandler(specItem) {
+      this._specItem = specItem
+    },
+    addSpecTypeHandler() {
+      if (this.form2.specs.length >= 2) {
+        this.$message({
+          type: 'warning',
+          message: '只允许新增两种商品尺寸'
+        })
+        return
+      }
+      this.form2.specs.push({
+        specType: '',
+        image: '',
+        data: []
+      })
+    },
+    deleteSpecTypeHandler(index) {
+      this.form2.specs.splice(index, 1)
+    },
+    deleteSpecHandler(specDatas, index) {
+      specDatas.splice(index, 1)
     }
   }
 }
